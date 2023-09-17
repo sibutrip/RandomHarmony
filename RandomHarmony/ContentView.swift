69bc0ad1175f82696dcfc0d98f756bae02b0e23e
Cory Tripathy <ctripathy0@gmail.com>
2023-09-17 13:20:09 -0500

    Initial Commit

---
+++ RandomHarmony/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomHarmony
+//
+//  Created by Cory Tripathy on 9/17/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomHarmony/RandomHarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomHarmonyApp.swift
+//  RandomHarmony
+//
+//  Created by Cory Tripathy on 9/17/23.
+//
+
+import SwiftUI
+
+@main
+struct RandomHarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
