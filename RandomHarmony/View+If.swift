085a8a38b422892febf51c92a5f39ed4f23bdee9
Cory Tripathy <ctripathy0@gmail.com>
2023-10-05 11:13:33 -0400

    note offset in cluster chords

---
+++ RandomHarmony/View+If.swift
@@ -0,0 +1,18 @@
+//
+//  View+If.swift
+//  RandomHarmony
+//
+//  Created by Cory Tripathy on 10/4/23.
+//
+
+import SwiftUI
+
+struct View_If: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    View_If()
+}
